5f9a1a2254c5a1aff4e3a3555e62d6436be62323
Kamalesh <kamalesh5112000@pec.edu.com>
2023-06-25 04:24:26 +0530

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
-    
-  });
-  
-  app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-  });
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(3000);
