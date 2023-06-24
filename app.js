c439896e005bad22f42682e8ab172819cd450f11
Kamalesh <kamalesh5112000@pec.edu.com>
2023-06-25 04:19:14 +0530

    second commit

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("in the middleware");
-    next();
-
-});
-app.use((req,res,next)=>{
-    console.log("in another middleware");
-    res.send( { key1: 10 })
-
-});
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');
+    
+  });
+  
+  app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+  });
 
 app.listen(3000);
